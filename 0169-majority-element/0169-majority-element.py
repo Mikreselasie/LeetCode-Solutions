ddccf4b1ee34ea2270104534c398db466809d22a
Mikreselasie Abiy <111493840+Mikreselasie@users.noreply.github.com>
2025-02-04 16:04:01 +0300

    Time: 1 ms (87.03%), Space: 19.8 MB (16.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,6 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        major = len(nums)/2
 
         nums_count = Counter(nums)
         nums_sort = sorted(nums_count.items(),key = lambda item: item[1],reverse = True)
