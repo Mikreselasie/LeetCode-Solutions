b0939c27173d04794c0d3ff046ea0b78e21471dd
Mikreselasie Abiy <111493840+Mikreselasie@users.noreply.github.com>
2025-02-05 17:23:11 +0300

    Time: 0 ms (100%), Space: 17.7 MB (75.88%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.py
+++ 1651-shuffle-string/1651-shuffle-string.py
@@ -4,10 +4,10 @@ class Solution:
         
         for i in range(len(s)):
             index_to_char_dict[indices[i]] = s[i]
-        sorted_dict = sorted(index_to_char_dict.items(), key=lambda x: x[0])
+        
         word = []
-        for index,char in sorted_dict:
-            word.append(char)
+        for i in range(len(indices)):
+            word.append(index_to_char_dict[i])
         return "".join(word)
 
         
