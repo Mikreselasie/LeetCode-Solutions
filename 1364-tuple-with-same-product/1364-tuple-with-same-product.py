929c3da2a382d9833ce3d39c299a4529b153c264
Mikreselasie Abiy <111493840+Mikreselasie@users.noreply.github.com>
2025-02-06 09:27:36 +0300

    Time: 394 ms (44.08%), Space: 46.3 MB (65.4%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.py
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -1,10 +1,10 @@
 class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
-        multiply_count = Counter()
+        multiply_count = {}
         count = 0
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
-                multiply_count[nums[i]*nums[j]] += 1
+                multiply_count[nums[i]*nums[j]] = multiply_count.get(nums[i]*nums[j],0) + 1
         
         for p,c in multiply_count.items():
             count += ((c)*(c-1)//2)*8
