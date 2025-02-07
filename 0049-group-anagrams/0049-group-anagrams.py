59e84d072db0f2ab1442be41cba98c66fa53fecb
Mikreselasie Abiy <111493840+Mikreselasie@users.noreply.github.com>
2025-02-07 10:09:45 +0300

    Time: 44 ms (6.61%), Space: 22.8 MB (11.94%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -10,30 +10,10 @@ class Solution:
                 tup[ord_v] = j
 
             tupled = tuple(tup)
-            # for char in word:
-            #     tup[ord_v] += 1
             my_dict[tupled] = my_dict.get(tupled, []) + [word]
 
         for count,words in my_dict.items():
             anagrams.append(words)
 
         return anagrams
-        # anagrams_paired = []
-        # dont_consider = []
-        # for i in range(len(strs)):
-        #     anagrams = []
-        #     count1 = Counter(strs[i])
-        #     for j in range(i,len(strs)):
-        #         if Counter(strs[j]) == count1 and j not in dont_consider:
-        #             anagrams.append(strs[j])
-        #             dont_consider.append(j)
-
-        #     if anagrams == []:
-        #         continue
-        #     else:
-        #         anagrams_paired.append(anagrams)
-        # return anagrams_paired     
-
-        
-
-        
+       
