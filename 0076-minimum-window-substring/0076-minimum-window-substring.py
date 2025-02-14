d31571881f197553c2a8ce0a4338e7ab356d1a01
Mikreselasie Abiy <111493840+Mikreselasie@users.noreply.github.com>
2025-02-14 11:44:57 +0300

    Time: 949 ms (5.01%), Space: 18.2 MB (75.38%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,30 +1,6 @@
 from collections import defaultdict
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # def valid(count_t,window):
-        #     for char in count_t:
-        #         if count_t[char] > window[char]:
-        #             return False
-        #     return True
-        # t_count = Counter(t)
-        # t_len = len(t)
-        # window = Counter()
-        # my_list = list()
-        # left = 0
-        # ans = [float("-inf"),float("inf")]
-
-        # for right in range(len(s)):
-        #     window[s[right]] += 1
-        #     while valid(t_count,window):
-        #         if right - left < ans[1] - ans[0]:
-        #             ans = [left,right]
-        #         window[s[left]] -= 1
-        #         left += 1
-        # if ans[0] == float("-inf"):
-        #     return ""
-        # return s[ans[0]:ans[1] + 1]
-
-
         t_count = Counter(t)
         s_count = Counter()
         left = 0
