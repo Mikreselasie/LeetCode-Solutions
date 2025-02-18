4e1be1b80072723a079dee2ce59544f9ae7fb4bc
Mikreselasie Abiy <111493840+Mikreselasie@users.noreply.github.com>
2025-02-18 17:14:40 +0300

    Time: 46 ms (10.97%), Space: 26 MB (32.97%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,23 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        running = 1
-        switch = False
-        not_running = 0
-        for num in nums:
-            if num == 0:
-                switch = True
-                not_running += 1
-                continue
-            running *= num
-        for i in range(len(nums)): 
-            if nums[i] == 0:
-                if not_running >= 2:
-                    continue
-                nums[i] = running
-                continue
-            if not switch:
-                nums[i] = running//nums[i]
-            else:
-                nums[i] = 0
-        return nums
+        suffix = [1]*len(nums)
+        prefix = [1]*len(nums)
+        answer = []
+        for i in range(1,len(nums)):
+            prefix[i] = prefix[i-1]*nums[i-1]
+            suffix[len(nums)-i-1] = suffix[len(nums)-i] * nums[len(nums)-i]
+        for i in range(len(nums)):
+            answer.append(prefix[i]*suffix[i])
+        return answer
+
         
