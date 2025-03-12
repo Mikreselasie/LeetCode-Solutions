20a430854e0f663190cab8e30e2dd26b6c6bb0f5
Mikreselasie Abiy <111493840+Mikreselasie@users.noreply.github.com>
2025-03-12 19:34:16 +0300

    Time: 0 ms (100%), Space: 17.9 MB (31.9%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,20 +1,15 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         my_dict = dict()
-        if n == 1:
-            return x
-        if x == 0:
-            return 0
-        if n == 0:
-            return 1
-        
-        def pow(x,n):
-            if n == 1:
-                return x
-            if ceil(n/2) not in my_dict: my_dict[ceil(n/2)] = pow(x, ceil(n/2))
-            if n//2 not in my_dict: my_dict[n//2] = pow(x, n//2)
-            return my_dict[ceil(n/2)]*my_dict[n//2]
-        answer = pow(x,abs(n))
+        def pow(x,m):
+            if m == 1: return x
+            if m == 0: return 1
+            if ceil(m/2) not in my_dict: my_dict[ceil(m/2)] = pow(x,ceil(m/2))
+            if m//2 not in my_dict: my_dict[m//2] = pow(x,m//2)
+
+            return my_dict[ceil(m/2)] * my_dict[m//2] 
+
+        ans = pow(x,abs(n))
         if n < 0:
-            return 1/answer
-        return answer
+            return 1/ans
+        return ans
