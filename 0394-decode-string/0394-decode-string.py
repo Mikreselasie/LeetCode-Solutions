5adf325016a723b00fc307b728f91150261c7824
Mikreselasie Abiy <111493840+Mikreselasie@users.noreply.github.com>
2025-03-19 09:43:37 +0300

    Time: 0 ms (100%), Space: 17.7 MB (84.61%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,30 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        idx = 0
+        def helper(idx):
+            stack = []
+            while idx < len(s):
+                if s[idx].isdigit():
+                    newidx = idx
+                    while idx<len(s) and s[idx].isdigit(): 
+                        idx += 1
+
+                    num = int(s[newidx:idx])
+                    ans = helper(idx)
+                    stack.extend(ans[0]*num)
+                    idx = max(ans[1],idx)
+                elif s[idx] != "[" and s[idx] != "]":
+                    stack.append(s[idx])
+
+                elif s[idx] == "]":
+                    return ["".join(stack), idx]
+                idx += 1
+                
+            return ["".join(stack), idx]
+        total = []
+        while idx < len(s):
+            ans = helper(idx)
+            idx = ans[1]+1
+            total.extend(ans[0])
+            
+        return "".join(total)
