781766841c282ffc13593729c6006be4526e61c9
Mikreselasie Abiy <111493840+Mikreselasie@users.noreply.github.com>
2025-03-19 06:59:48 +0300

    Time: 239 ms (6.68%), Space: 21.8 MB (20.05%) - LeetHub

---
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        def swapper(num):
+            if num == 0:
+                return 1
+            else:
+                return 0
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] == 0 and i < len(nums)-2:
+                for j in range(i,i+3):
+                    nums[j] = swapper(nums[j])
+                count += 1
+            elif nums[i] == 0 and i >= len(nums)-2:
+                return -1
+        return count
