0ec66d4647a5953d80c7d9e4147e2cbc3c603a63
Mikreselasie Abiy <111493840+Mikreselasie@users.noreply.github.com>
2025-03-24 13:52:07 +0300

    Time: 207 ms (31.48%), Space: 53.4 MB (14.11%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.py
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.py
@@ -1,22 +1,24 @@
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
-        meetings.sort(key = lambda x: (x[0],x[1]))
-        
+        meetings.sort(key = lambda x:(x[0],x[1]))
+
         top = meetings[0][1]
         prev_top = top
         bottom = meetings[0][0]
-        meets = top - bottom+1
+        meet_dur = top - bottom +1
 
+
+        # [[1,3],[2,4],[3,5]]    3 1 
         for i in range(1,len(meetings)):
             if meetings[i][0] <= top:
                 if meetings[i][1] <= top:
                     continue
                 else:
                     top = meetings[i][1]
-                    meets += top - prev_top
+                    meet_dur += top - prev_top
             else:
                 bottom = meetings[i][0]
                 top = meetings[i][1]
-                meets += top - bottom + 1
+                meet_dur += top - bottom + 1
             prev_top = top
-        return days - meets
+        return days - meet_dur
