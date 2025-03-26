b3388343d9973f307eb95575214f424e75db4a97
Mikreselasie Abiy <111493840+Mikreselasie@users.noreply.github.com>
2025-03-26 13:38:28 +0300

    Time: 156 ms (57.46%), Space: 38.9 MB (78.95%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.py
@@ -5,16 +5,12 @@ class Solution:
         for row in grid:
             for num in row:
                 nums.append(num)
-    
         nums.sort()
         median = nums[len(nums)//2]
         count = 0
 
         for num in nums:
-            if abs(num - median) % x != 0:
-                return -1
-            count += abs(num - median) // x
-        return count
-
-
-        
+            if abs(num - median) % x !=0:
+                return -1 
+            count += abs(num - median)//x
+        return count    
