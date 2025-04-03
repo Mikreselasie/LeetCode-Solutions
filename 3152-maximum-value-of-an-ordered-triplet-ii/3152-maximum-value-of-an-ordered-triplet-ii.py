c96edeb88c556743f546dc455c69695ba55ac9d1
Mikreselasie Abiy <111493840+Mikreselasie@users.noreply.github.com>
2025-04-03 10:12:47 +0300

    Time: 220 ms (36.86%), Space: 30.9 MB (6.83%) - LeetHub

--- 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.py
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.py
@@ -5,13 +5,13 @@ class Solution:
 
         for num in nums[1:]:
             prefix_max.append(max(num,prefix_max[-1]))
-
-        for num in nums[::-1]:
-            suffix_max.append(max(suffix_max[-1],num))
         
-        suffix_max = suffix_max[::-1]
+        for num in nums[::-1]:
+            suffix_max.append(max(num,suffix_max[-1]))
+        
         max_no = 0
-        print(suffix_max,prefix_max)
+        suffix_max = suffix_max[::-1]
+
         for i in range(1,len(nums)-1):
             max_no = max(max_no,(prefix_max[i]-nums[i])*suffix_max[i+1])
         return max_no
