097a6e77724d5b15e7ef84744159134f9b41dce5
Mikreselasie Abiy <111493840+Mikreselasie@users.noreply.github.com>
2025-04-04 15:35:13 +0300

    Time: 0 ms (100%), Space: 23.1 MB (37.49%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        left = 0
+        right = len(citations)
+        while left <= right:
+            mid = (left+right)//2
+            if len(citations)-bisect_left(citations, mid) >= mid: left = mid + 1
+            else: right = mid - 1
+        return right
