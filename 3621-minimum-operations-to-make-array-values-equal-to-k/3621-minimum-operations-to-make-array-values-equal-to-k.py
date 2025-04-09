dd7fefd18c6b696ce702c13bf98bca61313f4eca
Mikreselasie Abiy <111493840+Mikreselasie@users.noreply.github.com>
2025-04-09 17:53:17 +0300

    Time: 54 ms (98.35%), Space: 17.9 MB (9.43%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.py
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.py
@@ -1,16 +1,5 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        for num in nums:
-            if num < k: return -1
-        
-        counter = Counter(nums)
-
-        new = []
-        for num,val in counter.items():
-            new.append((num,val))
-        
-        new.sort()
-        for i in range(len(new)):
-            if new[i][0] == k:
-                return len(new) - (i+1)
-        return len(new)
+        nums = sorted(list(set(nums)))
+        if nums[0] < k: return -1
+        return len(nums) - (nums.index(k)+1) if k in nums else len(nums)
